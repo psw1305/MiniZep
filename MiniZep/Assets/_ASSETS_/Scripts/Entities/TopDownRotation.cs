04cda3e45649ddd8a7b9b487b066202c65753d94
psw1305 <shshck5@gmail.com>
2023-11-24 18:56:22 +0900

    Add ProCamera & Room
    - 써드파티 ProCamera로 플레이어 따라가는 카메라 구현
    - 타일맵 추가 및 프리팹화 (Room1, Room2)

---
+++ MiniZep/Assets/_ASSETS_/Scripts/Entities/TopDownRotation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TopDownRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownCharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+    }
+
+    private void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        characterRenderer.flipX = direction.x >= 0 ? characterRenderer.flipX = false : characterRenderer.flipX = true;
+    }
+}

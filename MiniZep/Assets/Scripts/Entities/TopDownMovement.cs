d3261ec833955ea75e651c02ca74bf33926c2348
psw1305 <shshck5@gmail.com>
2023-11-24 16:54:46 +0900

    Implement Character Input
    - 캐릭터 조작 구현 (움직임, 조준)
    - 캐릭터 애니메이션 추가 (Idle, Run)
    - 이미지 및 타일 리소스 추가

---
+++ MiniZep/Assets/Scripts/Entities/TopDownMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private Animator animator;
+
+    private TopDownCharacterController _controller;
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction *= speed;
+        _rigidbody.velocity = direction;
+
+        if (direction != Vector2.zero)
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+    }
+}

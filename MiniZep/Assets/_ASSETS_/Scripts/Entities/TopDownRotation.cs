ff967e4ca86bc8ff4fdac06cbf926f79a6c0fd07
psw1305 <shshck5@gmail.com>
2023-11-25 18:12:35 +0900

    Add Font&ThirdParty
    - 폰트 추가 및 에셋 생성 (던파비트체v2)
    - DOTween 추가

--- MiniZep/Assets/_ASSETS_/Scripts/Entities/TopDownRotation.cs
+++ MiniZep/Assets/_ASSETS_/Scripts/Entities/TopDownRotation.cs
@@ -18,15 +18,16 @@ public class TopDownRotation : MonoBehaviour
 
     public void OnAim(Vector2 mousePosition)
     {
-        FlipPlayer(mousePosition);
+        ApplyRotate(mousePosition);
     }
 
-    private void FlipPlayer(Vector2 mousePosition)
+    private void ApplyRotate(Vector2 mousePosition)
     {
         // 마우스 위치에 플레이어 위치를 빼야 기준점이 잡힘
         Vector2 direction = mousePosition - (Vector2)transform.position;
 
-        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        //float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        characterRenderer.flipX = direction.x <= 0;
     }
 }
